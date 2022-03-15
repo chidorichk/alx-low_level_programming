5171f504a8eefff911662634973d66a4adb00c1e
chidorichk <warakpesamiou@gmail.com>
2022-03-15 13:16:16 -0700

    conditional statement

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,28 @@
+
+0;136;0c#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ *main - that function print the alphabet
+ *with a new line
+ * Return: Always 0(Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'B'; c++)
+        {
+                putchar(c);
+        }
+	putchar('\n');
+	return (0);
+}
