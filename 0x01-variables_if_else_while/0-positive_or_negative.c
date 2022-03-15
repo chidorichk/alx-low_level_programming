2c9cf948105f64d2e81acd3a1fdc739b594aa69b
chidorichk <warakpesamiou@gmail.com>
2022-03-15 12:36:15 -0700

    conditional statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,25 @@
-conditional statement
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/*main: that function detect if a number is posive, negative or zero */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+	else
+	{
+	printf("%d is zero\n", n);
+	}
+	return (0);
+}
