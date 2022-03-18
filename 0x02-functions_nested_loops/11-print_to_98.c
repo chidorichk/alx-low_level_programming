8ea9724910e9d3eec4e17ffc70ae4b0f3ab94480
chidorichk <warakpesamiou@gmail.com>
2022-03-18 12:18:54 -0700

    back

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,6 +12,8 @@
 void print_to_98(int n)
 {
 int i;
+if (n <= 98)
+{
 for (i = n; i <= 98; i++)
 {
 	printf("%d", i);
@@ -19,4 +21,15 @@ for (i = n; i <= 98; i++)
 	printf(", ");
 }
 	printf("\n");
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+	printf("%d", i);
+	if (i != 98)
+	printf(", ");
+}
+	printf("\n");
+}
 }
