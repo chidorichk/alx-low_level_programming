a57d4e75df14b0ec8b9a8f8a9e18652b8e394ea8
chidorichk <warakpesamiou@gmail.com>
2022-03-18 11:46:45 -0700

    back

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *add - check whether a letter is lower or not
+ *@n1: parameters to add
+ *@n2:parameter to add
+ *Return: result
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
