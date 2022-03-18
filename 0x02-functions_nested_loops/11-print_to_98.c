21fb4cb8ae4b66b70f02beca7727e325e9ee0c40
chidorichk <warakpesamiou@gmail.com>
2022-03-18 12:05:48 -0700

    back

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *print_to_98 - print numbers
+ *@n: parameters to start with
+ *Return: nothing
+ */
+
+void print_to_98(n)
+{
+int i;
+for (i = n; i <= 98; i++)
+{
+	printf(n);
+	if (n != 98)
+	printf(", ");
+}
+}
