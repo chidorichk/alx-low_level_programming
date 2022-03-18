029f1f3d3229f444e89378119cd7b75f76724030
chidorichk <warakpesamiou@gmail.com>
2022-03-18 13:51:41 -0700

    back

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,11 +12,11 @@ int i, sum;
 sum = 0;
 for (i = 0; i < 1024; i++)
 {
-	if ((i % 3 == 0) || (i %5 == 0))
+	if ((i % 3 == 0) || (i % 5 == 0))
 	{
 	sum = sum + i;
 	}
 }
-	printf("%d", sum);
-	return 0;
+	printf("%d\n", sum);
+	return (0);
 }
