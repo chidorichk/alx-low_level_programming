bc7a149a99c821408a7f6080fd16831a66caca1a
chidorichk <warakpesamiou@gmail.com>
2022-03-22 21:12:37 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_array - set a variable to 98
+*@a: paramater to swap
+*@n: parameter
+*Return: length of string
+*/
+
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
